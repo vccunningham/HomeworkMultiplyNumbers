7d4893f7223b03788028b489ef9b0f648939b808
max-student <max-student@maxtrain.com>
2020-02-11 12:23:38 -0500

    Add project files.

---
+++ HomeworkMultiplyNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldProject {
+    class HomeworkMultiplyNumbers {
+        
+           
+                public void Start() { 
+
+            var product = 1;
+            for(var number = 1; number <= 10; number++) {
+                product = product * number;
+                }
+
+            Console.WriteLine($"Product of numbers 1 to 10 is {product}");
+
+            }
+        }
+    }
